76e17e8810430123fd245ac8c7a02b25c0970821
Timothee Boulet <timboulet@hotmail.fr>
2019-12-02 10:45:36 +0100

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,19 @@
+def fuel(mass):
+	return int(mass / 3.) - 2
+
+result = 0
+with open('Day01_input.txt', 'rb') as f:
+	for line in f.readlines():
+		result += fuel(int(line))
+
+print(f"First star result is {result}")
+
+result = 0
+with open('Day01_input.txt', 'rb') as f:
+	for line in f.readlines():
+		module_fuel = fuel(int(line))
+		while module_fuel > 0:
+			result += module_fuel
+			module_fuel = fuel(module_fuel)
+
+print(f"Second star result is {result}")
