3653d02c2f18ef7061ffb1632657247c9da461d8
Timothee Boulet <timboulet@hotmail.fr>
2019-12-07 00:53:58 +0100

    Update Day06.py

--- Day06.py
+++ Day06.py
@@ -1,30 +1,33 @@
 with open('Day06_input.txt', 'r') as f:
 	orbits = f.read().split('\n')
 
-#orbits = ['B)C', 'D)E', 'E)F', 'B)G', 'COM)B', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'C)D']
-orbits = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN']
-
 planets = {}
+fathers = {}
 for orb in [o for o in orbits if o!='']:
 	p1, p2 = orb.split(')')
+	fathers[p2] = p1
 	if p2 == 'SAN':
 		target = p1
-	if p1 == 'YOU':
+	if p2 == 'YOU':
 		source = p1
-	if p1 in planets:
-		planets[p1].add(p2)
-		if p2 in planets: planets[p1].update(planets[p2])
-	else:
-		planets[p1] = set([p2])
-		if p2 in planets: planets[p1].update(planets[p2])
+	planets[p1] = planets.get(p1, []) + [p2]
+	if p2 in planets:
+		planets[p1].extend(planets[p2])
 
 	for p in planets:
 		if p1 in planets[p]: 
-			planets[p].add(p2)
-			if p2 in planets: planets[p].update(planets[p2])
+			planets[p].append(p2)
+			if p2 in planets:
+				planets[p].extend(planets[p2])
 
 print(f"Result of first star is {sum(len(planets[p]) for p in planets)}")
 
-print([(p, len(planets[p])) for p in planets if 'SAN' in planets[p] and 'YOU' in planets[p]])
-father = min([(len(planets[p]), p) for p in planets if 'SAN' in planets[p] and 'YOU' in planets[p]])
-print(father)
+commun = min((p for p in planets if 'SAN' in planets[p] and 'YOU' in planets[p]), key=lambda p: len(planets[p]))
+c = 0
+while source != commun:
+	c += 1
+	source = fathers[source]
+while target != commun:
+	c += 1
+	target = fathers[target]
+print(f"Result of first star is {c}")
