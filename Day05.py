b09e570770e285a8ad1b134f6171a2f54453a592
Timothee Boulet <timboulet@hotmail.fr>
2019-12-11 19:01:12 +0100

    Add files via upload

--- Day05.py
+++ Day05.py
@@ -1,104 +1,92 @@
-with open("Day05_input.txt", "r") as f:
-	intcode_txt = f.read()
-
-def opcode(program, pointeur):
+def opcode_plus(program, pointeur=0, inputs=[], relative_base=0):
 	instruction = program[pointeur]
-	a = instruction % 100
-	if a in [1, 2]:
-		modes = str(instruction // 100)[::-1]
-		param_1 = program[program[pointeur+1]] if modes[0]=='0' else program[pointeur+1]
-		param_2 = program[program[pointeur+2]] if (len(modes)==1 or modes[1]=='0') else program[pointeur+2]
-		if a == 1: program[program[pointeur+3]] = param_1 + param_2
-		elif a == 2: program[program[pointeur+3]] = param_1 * param_2
-		pointeur += 4
-		return {"output": 1, "program": program, "pointeur": pointeur}
+	opcode = instruction % 100
+	modes = instruction // 100
 
-	elif a == 3:
-		program[program[pointeur+1]] = int(input("Input value?"))
-		pointeur += 2
-		return {"output": 1, "program": program, "pointeur": pointeur}
-
-	elif a == 4:
-		b = instruction // 100
-		b = program[pointeur+1] if b==1 else program[program[pointeur+1]]
-		pointeur += 2
-		return {"output": 2, "program": program, "pointeur": pointeur, "value": b}
-
-	elif a == 99:
-		return {"output": 0, "value": 99}
-
-	raise ValueError(f"a shouldn't be equal to {a}")
+	if opcode in [1, 2, 4, 5, 6, 7, 8, 9]:
+		if modes%10 == 1:
+			param_1 = program[pointeur+1]
+		else:
+			param_1 = program[program[pointeur+1]  + relative_base*(modes%10==2)]
+	if opcode in [1, 2, 5, 6, 7, 8]:
+		if modes//10 == 1:
+			param_2 = program[pointeur+2]
+		else:
+			param_2 = program[program[pointeur+2] + relative_base*(modes//10==2)]
 
 
-program = list(map(int, intcode_txt.split(',')))
-dico = {"pointeur": 0, "output": 1, "program": program}
-r = []
-c = 0
-while dico["output"] > 0:
-	dico = opcode(dico["program"], dico["pointeur"])
-	c += 1
-	if dico["output"] != 1:
-		r.append((c, dico["output"], dico["value"]))
+	if opcode in [1, 2, 3, 5, 6, 7, 8, 9]:
+		if opcode in [1, 2, 7, 8]:
+			program += [0] * max(0, program[pointeur+3]-len(program)+1)
+		if opcode == 3:
+			program += [0] * max(0, program[pointeur+1]-len(program)+1)
 
-assert r[-1][2] == 99
-assert r[-1][0] == r[-2][0] + 1
-assert all(v[2] == 0 for v in r[:-2])
-
-print(f"Result of first star is {r[-2][2]}")
-
-def opcode_plus(program, pointeur):
-	instruction = program[pointeur]
-	a = instruction % 100
-	if a in [1, 2, 5, 6, 7, 8]:
-		modes = str(instruction // 100)[::-1]
-		param_1 = program[program[pointeur+1]] if modes[0]=='0' else program[pointeur+1]
-		param_2 = program[program[pointeur+2]] if (len(modes)==1 or modes[1]=='0') else program[pointeur+2]
-		if a == 1: 
+		if opcode == 1: 
 			program[program[pointeur+3]] = param_1 + param_2
 			pointeur += 4
-		if a == 2: 
+		elif opcode == 2: 
 			program[program[pointeur+3]] = param_1 * param_2
 			pointeur += 4
-		if a == 5: 
+		elif opcode == 3:
+			program[program[pointeur+1]] = inputs.pop(0) if len(inputs) else int(input("Input value?"))
+			pointeur += 2
+		elif opcode == 5: 
 			pointeur = param_2 if param_1!=0 else pointeur+3
-		if a == 6: 
+		elif opcode == 6: 
 			pointeur = param_2 if param_1==0 else pointeur+3
-		if a == 7:
+		elif opcode == 7:
 			program[program[pointeur+3]] = 1 if param_1 < param_2 else 0
 			pointeur += 4
-		if a == 8: 
+		elif opcode == 8: 
 			program[program[pointeur+3]] = 1 if param_1 == param_2 else 0
 			pointeur += 4
+		elif opcode == 9:
+			relative_base += param_1
+			pointeur += 2
 
-		return {"output": 1, "program": program, "pointeur": pointeur}
+		return {"output": 1, "program": program, "pointeur": pointeur, "inputs": inputs, "relative_base": relative_base}
 
-	elif a == 3:
-		program[program[pointeur+1]] = int(input("Input value?"))
+
+	elif opcode == 4:
 		pointeur += 2
-		return {"output": 1, "program": program, "pointeur": pointeur}
+		return {"output": 2, "program": program, "pointeur": pointeur, "inputs": inputs, "relative_base": relative_base, "value": param_1}
 
-	elif a == 4:
-		b = instruction // 100
-		b = program[pointeur+1] if b==1 else program[program[pointeur+1]]
-		pointeur += 2
-		return {"output": 2, "program": program, "pointeur": pointeur, "value": b}
+	elif opcode == 99:
+		return {"output": 0}
 
-	elif a == 99:
-		return {"output": 0, "value": 99}
+	raise ValueError(f"opcode shouldn't be equal to {opcode}")
 
-	raise ValueError(f"a shouldn't be equal to {a}")
+if __name__ == '__main__':
+	with open("Day05_input.txt", "r") as f:
+		intcode_txt = f.read()
+
+	program = list(map(int, intcode_txt.split(',')))
+	dico = {"pointeur": 0, "output": 1, "program": program}
+	r = []
+	c = 0
+	while dico["output"] > 0:
+		dico = opcode_plus(dico["program"], dico["pointeur"])
+		c += 1
+		if dico["output"] != 1:
+			r.append((c, dico))
+
+	assert r[-1][1]["output"] == 0
+	assert r[-1][0] == r[-2][0] + 1
+	assert all(v[1]["value"] == 0 for v in r[:-2])
+
+	print(f'Result of first star is {r[-2][1]["value"]}')
 
 
-program = list(map(int, intcode_txt.split(',')))
-dico = {"pointeur": 0, "output": 1, "program": program}
-r = []
-c = 0
-while dico["output"] > 0:
-	dico = opcode_plus(dico["program"], dico["pointeur"])
-	c += 1
-	if dico["output"] != 1:
-		r.append((c, dico["output"], dico["value"]))
+	program = list(map(int, intcode_txt.split(',')))
+	dico = {"pointeur": 0, "output": 1, "program": program}
+	r = []
+	c = 0
+	while dico["output"] > 0:
+		dico = opcode_plus(dico["program"], dico["pointeur"])
+		c += 1
+		if dico["output"] != 1:
+			r.append((c, dico))
 
-assert r[-1][2] == 99
-assert len(r) == 2
-print(f"Result of second star is {r[-2][2]}")
+	assert r[-1][1]["output"] == 0
+	assert len(r) == 2
+	print(f'Result of second star is {r[-2][1]["value"]}')
--- Day07.py
+++ Day07.py
@@ -1,55 +1,70 @@
 from Day05 import opcode_plus
 import itertools
 
-def thruster(program, sequences, first_input=0, feedback=False):
-	#print(f"Sequence is {sequences}")
-	inputs = list(sequences)
-	inputs.insert(1, first_input)
-	last_E = None
+def thruster(program, sequences, first_input=0, feedback=False, log=False):
+	amps = [Amplifier(program[:], s, 0) for s in sequences]
+	output_value = int(first_input)
 	for i in itertools.cycle(range(len(sequences))):
-		dico = {"pointeur": 0, "output": 1, "program": program, "inputs": inputs}
+		output_value = amps[i].run(output_value, log)
+		if feedback==False and (i+1) % len(sequences) == 0:
+			break
+		if feedback and amps[-1].done:
+			break
+	return amps[-1].value
+
+
+def find_max_thruster(program, range_iter=range(5), first_input=0, feedback=False, log=False):
+	return max((thruster(program, seq, first_input, feedback, log), seq) for seq in itertools.permutations(range_iter))
+
+class Amplifier:
+	def __init__(self, program, setting=None, pointeur=0, relative_base=0):
+		self.program = program
+		if setting is not None:
+			self.inputs = [setting]
+		else:
+			self.inputs = []
+		self.pointeur = pointeur
+		self.relative_base = relative_base
+		self.done = False
+		self.value = None
+
+	def run(self, value=None, log=False):
+		if value is not None:
+			self.inputs.append(value)
+		dico = {"output": 1, "program": self.program, "pointeur": self.pointeur, "inputs": self.inputs, "relative_base": 0}
 		while dico["output"] == 1:
-			dico = opcode_plus(dico["program"], dico["pointeur"], inputs=dico["inputs"])
-		inputs = dico["inputs"]
-		inputs.insert(1, dico["value"])
-		if (i+1)%len(sequences)==0:
-			last_E = dico["value"]			
-			inputs = list(sequences)
-			inputs.insert(1, dico["value"])
-		if not feedback and i+1==len(sequences):
-			break
-		if feedback and dico["output"] == 0:
-			break
-	#print(f'For sequence {sequences}, thruster is {last_E}')
-	return last_E
+			dico = opcode_plus(dico["program"], dico["pointeur"], dico["inputs"], dico["relative_base"])
+		
+		if dico["output"] == 0:
+			self.done = True
+			return self.value
 
-
-def find_max_thruster(program, range_iter=range(5), first_input=0, feedback=False):
-	best = (0, [])
-	for seq in itertools.permutations(range_iter):
-		best = max(best, (thruster(program, seq, first_input, feedback), list(seq)))
-	return best
+		self.value = dico["value"]
+		self.program = dico["program"]
+		self.pointeur = dico["pointeur"]
+		self.relative_base = dico["relative_base"]
+		self.inputs = dico["inputs"]
+		return self.value
 
 
 if __name__ == '__main__':
 	with open('Day07_input.txt', 'r') as f:
 		program = f.read().strip()
 
-	assert find_max_thruster([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]) == (43210, [4,3,2,1,0])
-	assert find_max_thruster([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]) == (54321, [0,1,2,3,4])
-	assert find_max_thruster([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]) == (65210, [1,0,4,3,2])
+	assert find_max_thruster([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]) == (43210, (4,3,2,1,0))
+	assert find_max_thruster([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]) == (54321, (0,1,2,3,4))
+	assert find_max_thruster([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]) == (65210, (1,0,4,3,2))
 
 	program = list(map(int, program.split(',')))
 	print(f"Result of first star is {find_max_thruster(program)[0]}")
 
-	input("Wait")
-
 	program_1 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
 	assert thruster(program_1, sequences=(9,8,7,6,5), feedback=True) == 139629729
-	assert find_max_thruster(program_1, range_iter=range(5,10), feedback=True) == (139629729, [9,8,7,6,5])
+	assert find_max_thruster(program_1, range_iter=range(5,10), feedback=True) == (139629729, (9,8,7,6,5))
+
 
 	program_2 = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
 	assert thruster(program_2, sequences=(9,7,8,5,6), feedback=True) == 18216
-	assert find_max_thruster(program_2, range_iter=range(5,10), feedback=True) == (18216, [9,7,8,5,6])
+	assert find_max_thruster(program_2, range_iter=range(5,10), feedback=True) == (18216, (9,7,8,5,6))
 
 	print(f"Result of second star is {find_max_thruster(program, range_iter=range(5,10), feedback=True)[0]}")
