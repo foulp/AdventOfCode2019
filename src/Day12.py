8839aafb56322d41259a38c0ae9c5c35ce238fef
tboulet <tboulet@sidetrade.com>
2020-01-10 15:02:41 +0100

    Day12 part2

--- src/Day12.py
+++ src/Day12.py
@@ -1,7 +1,9 @@
 import itertools
+import numpy as np
 import re
 
-REGEX = '-?\d+'
+REGEX = r'-?\d+'
+
 
 class Moon:
 	def __init__(self, pos):
@@ -12,11 +14,6 @@ class Moon:
 		self.vx = 0
 		self.vy = 0
 		self.vz = 0
-
-	def identical(self, m):
-		if self.__dict__ == m__dict__:
-			return True
-		return False
 
 	def update_position(self):
 		self.x += self.vx
@@ -37,19 +34,19 @@ class Moon:
 	def get_total_energy(self):
 		return self.get_potential_energy() * self.get_kinetic_energy()
 
+
 class System:
 	def __init__(self, moons):
 		self.m1 = Moon(re.findall(REGEX, moons[0]))
 		self.m2 = Moon(re.findall(REGEX, moons[1]))
 		self.m3 = Moon(re.findall(REGEX, moons[2]))
 		self.m4 = Moon(re.findall(REGEX, moons[3]))
-		self.history = []
+		self.starting_state = [Moon(re.findall(REGEX, moons[i])) for i in range(4)]
 
 	def get_moons(self):
 		return [self.m1, self.m2, self.m3, self.m4]
 
 	def update(self):
-		self.save_state()
 		for m1, m2 in itertools.combinations(self.get_moons(), 2):
 			gx = -1 if m1.x > m2.x else 1 if m1.x < m2.x else 0
 			gy = -1 if m1.y > m2.y else 1 if m1.y < m2.y else 0
@@ -61,17 +58,12 @@ class System:
 		for m in self.get_moons():
 			m.update_position()
 
-
-	def save_state(self):
-		state = []
-		for m in self.get_moons():
-			state.extend([m.x, m.y, m.z, m.vx, m.vy, m.vz])
-		self.history.append(state)
-
-	def equal_previous_state(self):
-		if [int(getattr(m, att)) for m in [self.m1, self.m2, self.m3, self.m4] for att in m.__dict__] in self.history:
-			return True
-		return False
+	def equal_previous_state_dim(self, dim):
+		for i in range(1, 5):
+			state = self.starting_state[i-1]
+			if eval(f'state.{dim}') != eval(f'self.m{i}.{dim}') or eval(f'state.v{dim}') != eval(f'self.m{i}.v{dim}'):
+				return False
+		return True
 
 	def get_total_energy(self):
 		return sum(m.get_total_energy() for m in self.get_moons())
@@ -84,15 +76,19 @@ def final_energy(moons, time_steps):
 
 	return system.get_total_energy()
 
+
 def identical_state(moons):
 	system = System(moons)
 	time_steps = 0
-	history = []
-	while True:
+	dim = {'x': 0, 'y': 0, 'z': 0}
+	while any(dim[d] == 0 for d in dim):
 		system.update()
 		time_steps += 1
-		if system.equal_previous_state():
-			return time_steps
+		for v in [k for k in dim.keys() if dim[k] == 0]:
+			if system.equal_previous_state_dim(v):
+				dim[v] = time_steps
+	return np.lcm.reduce(list(dim.values()), dtype=np.int64)
+
 
 if __name__ == '__main__':
 	with open('../inputs/Day12_input.txt', 'r') as f:
@@ -101,12 +97,13 @@ if __name__ == '__main__':
 	moons_1 = ["<x=-1, y=0, z=2>", "<x=2, y=-10, z=-7>", "<x=4, y=-8, z=8>", "<x=3, y=5, z=-1>"]
 	assert final_energy(moons_1, 10) == 179
 
-	moons_2 =['<x=-8, y=-10, z=0>', '<x=5, y=5, z=10>', '<x=2, y=-7, z=3>', '<x=9, y=-8, z=-3>']
+	moons_2 = ['<x=-8, y=-10, z=0>', '<x=5, y=5, z=10>', '<x=2, y=-7, z=3>', '<x=9, y=-8, z=-3>']
 	assert final_energy(moons_2, 100) == 1940
 
 	print(f"The result of first star is {final_energy(moons, 1000)}")
 
 	assert identical_state(moons_1) == 2772
+	print("Assert 1 OK")
 	assert identical_state(moons_2) == 4686774924
-
+	print("Assert 2 OK")
 	print(f"The result of second star is {identical_state(moons)}")
