929362849b8e6116eddecc5798a4af8bf65dc9c4
tboulet <tboulet@sidetrade.com>
2020-03-05 18:35:59 +0100

    Day19 part two

--- src/Day19.py
+++ src/Day19.py
@@ -13,7 +13,19 @@ if __name__ == '__main__':
             value = amp.run(values=[i, j])
             if value == 1:
                 compteur += 1
-
     print(f"The result of first star is {compteur}")
 
-    print(f"The result of second star is {0}")
+    x, y = 4, 3  # First angle of the beam, beam is empty between (0, 0) and (4, 3)
+    size = 100
+    while True:
+        value = Amplifier(program[:]).run(values=[x+size-1, y])
+        if value == 1:
+            value = Amplifier(program[:]).run(values=[x, y+size-1])
+            if value == 1:
+                break
+            else:
+                x += 1
+        else:
+            y += 1
+
+    print(f"The result of second star is {10000 * x + y}")
