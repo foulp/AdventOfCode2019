c5066e4ff81cb2f4ead0da22f1e527a4c48b7a32
foulp <timbolet@hotmail.fr>
2020-04-05 02:38:22 +0200

    Day22 part two start

--- src/Day22.py
+++ src/Day22.py
@@ -3,30 +3,26 @@ class SpaceCard:
         self.pos = pos
         self.deck_size = size_of_deck
 
-    def cut(self, n):
-        if n > 0:
-            if n <= self.pos:
-                self.pos -= n
-            else:
-                self.pos += self.deck_size - n
-        else:
-            if self.pos >= self.deck_size + n:
-                self.pos -= self.deck_size + n
-            else:
-                self.pos -= n
+    def cut(self, n, order=1):
+        self.pos = (self.pos - order * n) % self.deck_size
 
-    def deal_with_increment(self, n):
-        self.pos = (self.pos * n) % self.deck_size
+    def deal_with_increment(self, n, order=1):
+        if order == 1:
+            self.pos = (self.pos * n) % self.deck_size
+        else:
+            while self.pos % n:
+                self.pos += self.deck_size
+            self.pos //= n
 
     def deal_into_new_stack(self):
         self.pos = self.deck_size - self.pos - 1
 
-    def shuffle(self, commands):
-        for technique in commands:
+    def shuffle(self, commands, order=1):
+        for technique in commands[::order]:
             if technique.startswith('deal with increment'):
-                self.deal_with_increment(int(technique.split()[-1]))
+                self.deal_with_increment(int(technique.split()[-1]), order)
             elif technique.startswith('cut'):
-                self.cut(int(technique.split()[-1]))
+                self.cut(int(technique.split()[-1]), order)
             elif technique.startswith('deal into new stack'):
                 self.deal_into_new_stack()
             else:
@@ -41,13 +37,12 @@ if __name__ == '__main__':
     space_card.shuffle(commands_list)
     print(f"The result of first star is {space_card.pos}")
 
-# WRONG
     n_turns = 101741582076661
     deck_size = 119315717514047
     space_card = SpaceCard(2020, deck_size)
     already_met = [space_card.pos]
     for _ in range(n_turns):
-        space_card.shuffle(commands_list)
+        space_card.shuffle(commands_list, order=-1)
         if space_card.pos not in already_met:
             already_met.append(space_card.pos)
         else:
