608061f4c280d9a0eeb9b3c982a68de016ff99c0
foulp <timbolet@hotmail.fr>
2020-05-21 02:35:34 +0200

    Day22 part two end

--- src/Day22.py
+++ src/Day22.py
@@ -20,7 +20,7 @@ class Shuffle:
     def full_shuffle(self, commands):
         # With f(x) = a'x + b' % deck_size, and g(x) = ax + b % deck_size
         # f(g(x)) = a'ax + (a'b + b') % deck_size
-        a, b = 1, 0
+        full_a, full_b = 1, 0
         for technique in commands:
             if technique.startswith('deal with increment'):
                 aa, bb = self.deal_with_increment(int(technique.split()[-1]))
@@ -31,9 +31,9 @@ class Shuffle:
             else:
                 raise ValueError(f"Did not understand command: {technique}")
 
-            b = (bb + aa * b) % self.deck_size
-            a = (a * aa) % self.deck_size
-        return a, b
+            full_b = (bb + aa * full_b) % self.deck_size
+            full_a = (full_a * aa) % self.deck_size
+        return full_a, full_b
 
 
 if __name__ == '__main__':
@@ -49,21 +49,19 @@ if __name__ == '__main__':
     # Solve a*x+b = end_pos [deck_size]
     # => a*x = (end_pos-b) [deck_size]
     # => x = (end_pos-b) * inv_mod(a, deck_size) [deck_size] if GCD(a, deck_size) == 1
-    assert (answer - b) * pow(a, deck_size-2, deck_size) % deck_size
+    # NB : inv_mod(a, m) is x in range(0, m) such that a*x = 1 [m], exists iff GCD(a, m) = 1
+    # NB : inv_mod(a, deck_size) = pow(a, deck_size-2, deck_size) valid iff deck_size is prime
+    assert start_pos == (answer - b) * pow(a, deck_size-2, deck_size) % deck_size
 
     n_turns = 101741582076661
     deck_size = 119315717514047
     end_pos = 2020
-    already_met = [(a, b)]
-    for _ in range(n_turns-1):
-        b = (b + a * b) % deck_size
-        a = (a * a) % deck_size
-        if (a, b) in already_met:
-            break
-        else:
-            already_met.append((a, b))
+    shuffle = Shuffle(deck_size)
+    a, b = shuffle.full_shuffle(commands_list)
+    # f^-1(x) = inv_mod(a, deck_size) * (x-b) % deck_size
+    # f^n(x) = a**n * x + b * (1 + a + a**2 + a**3 + ... + a**(n-1)) % deck_size
+    a_final = pow(a, n_turns, deck_size)
+    b_final = b * (pow(a, n_turns, deck_size) - 1) * pow(a-1, deck_size-2, deck_size) % deck_size
 
-    a_final, b_final = already_met[n_turns % len(already_met)]
-    print(a_final, b_final)
     answer = (end_pos - b_final) * pow(a_final, deck_size-2, deck_size) % deck_size
     print(f"The result of second star is {answer}")
