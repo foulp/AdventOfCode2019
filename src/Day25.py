ccea6b65b4cb7a2fa7302faddde97be785a07be7
foulp <timbolet@hotmail.fr>
2020-05-28 02:39:38 +0200

    Day25 part one

---
+++ src/Day25.py
@@ -0,0 +1,26 @@
+from src.lib.intcode import Amplifier
+from src.Day17 import Scaffold
+
+if __name__ == '__main__':
+    with open('../inputs/Day25_input.txt', 'r') as f:
+        program = list(map(int, f.read().split(',')))
+
+    amp = Amplifier(program[:])
+    line = ''
+    while amp.done is False:
+        case = amp.run()
+        if case == ord('\n'):
+            print(line)
+            if line == 'Command?':
+                amp.inputs.extend(list(map(ord, input("Type command") + '\n')))
+            line = ''
+            continue
+        line += chr(case)
+
+    # 					        SICK BAY
+    # KITCHEN (infinite loop)	PASSAGES (astrolabe)	HOT CHOCOLATE FOUTAIN	ENGINEERING (escape pod)	NAVIGATION (coin)	            STORAGE (molten lava)
+    # HOLODECK			                                                        HULL BREACH	CORRIDOR (cake)	GIFT WRAPPING CENTER (photons)	OBSERVATORY (giant electromagnet)	SECURITY CHECKPOINT
+    # WARP DRIVE MAINTENANCE (sand)			ARCADE (food ration)	HALLWAY (weather machine)			                                                                        PRESSURE SENSITIVE FLOOR
+    # 		CREW QUARTERS (jam)	            SCIENCE LAB (ornament)	STABLES
+    # Food ration Astrolabe, Ornament, Weather machine is needed for the weight correction
+    print(f"The result of second star is {0}")
